c1c25d9ea8792db08310a97ee815002af45277a3
Petriix <85133760+PetriixD@users.noreply.github.com>
2023-04-16 23:00:41 +0200

    refactor

--- watcher.py
+++ watcher.py
@@ -1,47 +1,13 @@
+#!/usr/bin/env python
+
+from dotenv import load_dotenv
+load_dotenv()
 import os
-import psutil
-import smtplib
-from tabulate import tabulate
-from email.mime.text import MIMEText
-
-disk_usage = os.popen("df --output=avail -m / | awk 'NR==2 {print $1}'").read().strip()
-
-process_count = os.popen("ps -A | wc -l").read().strip()
-
-cpu_util = psutil.cpu_percent()
-
-ram = psutil.virtual_memory()
-ram_util = ram.percent
-
-current_users_count = os.popen("who | wc -l").read().strip()
-
-who_output = os.popen('who').read().strip()
-user_lines = who_output.split('\n')
-user_columns = ", ".join([line.split()[0] for line in user_lines])
+from email_helpers import get_email_message, send_email
+from utilities import get_monitoring_data, get_monitoring_data2, load_html
  
-ps_output = os.popen('ps').read().strip() 
-ps_lines = ps_output.split('\n')
-ps_columns = ", ".join([line.split()[3] for line in ps_lines])
-
-table = [
-    ['Disk usage (MB):', disk_usage],
-    ['Process count /w root:', process_count],
-    ['Current cpu usage (%):', cpu_util],
-    ['Current ram usage(%):', ram_util],
-    ['Current users count:', current_users_count],
-    ['Current user list:', user_columns],
-    ['Current running processes:', ps_columns]
-]
-
-formatted_table = tabulate(table, headers=['Metric', 'Value'], stralign=("left",))
-
-msg = MIMEText(formatted_table)
-msg['Subject'] = 'System status report'
-msg['From'] = 'petriixw@gmail.com'
-msg['To'] = 'peterdano08@gmail.com'
-
-s = smtplib.SMTP('smtp.gmail.com', 587)
-s.starttls()
-s.login('petriixw@gmail.com', 'priv-key')
-s.sendmail(msg['From'], msg['To'], msg.as_string())
-s.quit()
+data = get_monitoring_data()
+data2 = get_monitoring_data2()
+html = load_html(data, data2)
+msg = get_email_message('System status report', os.environ["FROM"], os.environ["TO"], html)
+send_email(os.environ["FROM"], os.environ["PASS"], msg)
